22440d3e8d9fd3ebfaea7ba0c111bbdfff327ab2
Nanamoriyama <nanamoriyama@outlook.jp>
2025-01-27 14:45:21 +0100

    install expo router

---
+++ src/app/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+
+const Index = (): JSX.Element => {
+  return (
+    <View>
+      <Text>Hello Worldaaa</Text>
+    </View>
+  );
+};
+export default Index;
